dfb817f51a87192f189a0ea0628a30f0848db02a
vkfkstptkd1 <vkfkstptkd1@naver.com>
2021-07-24 19:50:29 +0900

    Component Life Cycle

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from "react";
-import PropTypes from "prop-types";
 
 class App extends React.Component{
+    //constructor(){} App class가 호출되면 바로 호출 (생성자) --render 전  
+    //componentDidMount(){} component가 처음 render 할때 호출--render 후
+    //componentDidUpdate(){} component가 update된 후 호출--render 함수 안에 무언가 바뀌면 호출
+    //componentWillUnmount(){} component가 떠날 때 호출
     state = {//동적 데이터 사용하기위해 사용하는 object.
         count: 0
     };
@@ -16,6 +19,7 @@ class App extends React.Component{
     //current : this.state.count라고 명시해도 되지만, 외부 state에 의존하지 않기 위해서 (??) current 함수를 사용하는게 훨씬 더 효율적임.
 
     render(){
+        console.log("i am rendering");
         return (
         <div>
             <h1>The number is {this.state.count}</h1>
