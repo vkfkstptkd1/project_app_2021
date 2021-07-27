29af0a9bb074bbd98c4c2770208478cb13aac88b
vkfkstptkd1 <vkfkstptkd1@naver.com>
2021-07-27 20:43:47 +0900

    Planning the Movie Component

--- src/App.js
+++ src/App.js
@@ -5,31 +5,21 @@ class App extends React.Component{
     //componentDidMount(){} component가 처음 render 할때 호출--render 후
     //componentDidUpdate(){} component가 update된 후 호출--render 함수 안에 무언가 바뀌면 호출
     //componentWillUnmount(){} component가 떠날 때 호출
-    state = {//동적 데이터 사용하기위해 사용하는 object.
-        count: 0
-    };
-    
-    add=()=>{
-        this.setState(current=>({count: current.count + 1}));
-    };
-    minus=()=>{
-        this.setState(current=>({count: current.count - 1}));
-    };
     //setState : state를 refresh하고 render을 refresh해주는 함수, 
     //current : this.state.count라고 명시해도 되지만, 외부 state에 의존하지 않기 위해서 (??) current 함수를 사용하는게 훨씬 더 효율적임.
-
+    state = {
+        isLoading : true,
+        movies : []
+    }
+    componentDidMount() {
+        setTimeout(() => {//일정 시간 뒤 실행시키는 javascript 타이머 함수
+            this.setState({ isLoading: false }); //setState 내부에는 state에 정의되어있지 않은 변수 그냥 써놓아도 에러 안생김
+        },6000);//6초후 실행
+    }
     render(){
-        console.log("i am rendering");
-        return (
-        <div>
-            <h1>The number is {this.state.count}</h1>
-            <button onClick = {this.add}>add</button>
-            <button onClick={this.minus}>minus</button>
-        </div>/* 만약 this.add()의 형태라면 실행하자마자 console창에 add출력
-                 그냥 this.add이면 클릭할때만 출력, ()는 '즉시,바로' 를 의미 */
-        );
+        const { isLoading } = this.state;
+        return <div>{ isLoading ? "Loading ..." : "We are ready" }</div>;
     };
 };
-
 
 export default App;
