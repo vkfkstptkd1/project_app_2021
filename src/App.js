f0f8eb4abb0644c8ca4684210e55065938177efe
vkfkstptkd1 <vkfkstptkd1@naver.com>
2021-07-30 15:21:45 +0900

    Fetching Movies from API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import axios from "axios";
 class App extends React.Component{
     //constructor(){} App class가 호출되면 바로 호출 (생성자) --render 전  
     //componentDidMount(){} component가 처음 render 할때 호출--render 후
@@ -7,14 +7,16 @@ class App extends React.Component{
     //componentWillUnmount(){} component가 떠날 때 호출
     //setState : state를 refresh하고 render을 refresh해주는 함수, 
     //current : this.state.count라고 명시해도 되지만, 외부 state에 의존하지 않기 위해서 (??) current 함수를 사용하는게 훨씬 더 효율적임.
+    
     state = {
         isLoading : true,
         movies : []
     }
-    componentDidMount() {
-        setTimeout(() => {//일정 시간 뒤 실행시키는 javascript 타이머 함수
-            this.setState({ isLoading: false }); //setState 내부에는 state에 정의되어있지 않은 변수 그냥 써놓아도 에러 안생김
-        },6000);//6초후 실행
+    getMovies = async () => {//async -> 이 함수는 비동기, ="너는 이 함수를 기다려야 해" axios는 느리기때문에..
+        const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");//그래서 async 안에 뭘기다려? await 이후에 나오는걸! axios가 끝날때까지 기다렸다가 계속해
+    }
+    async componentDidMount(){//mount이후
+        this.getMovies();
     }
     render(){
         const { isLoading } = this.state;
